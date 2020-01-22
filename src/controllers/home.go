fd82b03b76cac38d5bcc128edad36b2a75fa799d
jeanlucc <jeanlucc@users.noreply.github.com>
2020-01-22 18:42:32 +0100

    Make home more simple

--- src/controllers/home.go
+++ src/controllers/home.go
@@ -2,15 +2,8 @@ package controllers
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/theodo/scalab/config"
 )
 
-type homeDataResponse struct {
-	Name        string
-	DatabaseUrl string
-}
-
 func Home(c echo.Context) error {
-	cfg := config.Cfg
-	return c.Render(200, "home.html", homeDataResponse{Name: "JLC", DatabaseUrl: cfg.Database.Url})
+	return c.Render(200, "home.html", new(interface{}))
 }
